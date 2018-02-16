6e584e45ca3dbfbf6fe5ad7e1d4539c4fa6248d5
Amit <pramanikamit093@gmail.com>
2018-02-16 19:32:50 +0530

    initial

---
+++ app/src/main/java/com/example/admin/searchviewsample/AnimalNames.java
@@ -0,0 +1,18 @@
+package com.example.admin.searchviewsample;
+
+/**
+ * Created by Admin on 16-02-2018.
+ */
+
+public class AnimalNames {
+    private String animalName;
+
+    public AnimalNames(String animalName) {
+        this.animalName = animalName;
+    }
+
+    public String getAnimalName() {
+        return this.animalName;
+    }
+
+}
---
+++ app/src/main/java/com/example/admin/searchviewsample/MainActivity.java
@@ -0,0 +1,60 @@
+package com.example.admin.searchviewsample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ListView;
+import android.widget.SearchView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    // Declare Variables
+    ListView list;
+    ListViewAdapter adapter;
+    SearchView editsearch;
+    String[] animalNameList;
+    ArrayList<AnimalNames> arraylist = new ArrayList<AnimalNames>();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        list = (ListView) findViewById(R.id.list_view);
+        editsearch = (SearchView) findViewById(R.id.search_view);
+
+        animalNameList = new String[]{"Lion", "Tiger", "Dog",
+                "Cat", "Tortoise", "Rat", "Elephant", "Fox",
+                "Cow","Donkey","Monkey"};
+
+        for (int i = 0; i < animalNameList.length; i++) {
+            AnimalNames animalNames = new AnimalNames(animalNameList[i]);
+            // Binds all strings into an array
+            arraylist.add(animalNames);
+        }
+
+        adapter = new ListViewAdapter(this, arraylist);
+
+        // Binds the Adapter to the ListView
+        if(list != null){
+            list.setAdapter(adapter);
+        }
+
+        editsearch.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                Toast.makeText(getBaseContext(), query, Toast.LENGTH_LONG).show();
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                String text = newText;
+                adapter.filter(text);
+                return false;
+            }
+        });
+
+    }
+}
